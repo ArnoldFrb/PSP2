9e2ee8c1aa080fc55185d5cf9a4b6143f9c2e960
Sebastian Santander <wsantander@unicesar.edu.co>
2020-12-20 14:37:48 -0500

    commit

--- src/logica/TareaComplementaria.java
+++ src/logica/TareaComplementaria.java
@@ -8,5 +8,13 @@ public class TareaComplementaria extends Tarea{
     public TareaComplementaria(String descripcionTarea, int duracionTarea, String faseProyecto, String ingeniero, Date fechaInicio, String RolRequerido) {
         super(descripcionTarea, duracionTarea, faseProyecto, ingeniero, fechaInicio, RolRequerido);
     }
+    
+    @Override
+    public String toString() {
+        return "Tarea Basico:" + super.toString();
+    }
 
+    public String datosParaArchivo(){
+        return super.getIdentificadorTarea()+";"+super.getDescripcionTarea()+";"+super.getDuracionTarea()+";"+super.getFaseProyecto()+";"+super.getIngeniero()+";"+super.getFechaInicio()+";"+super.getEstadoTarea()+";"+super.getRolRequerido()+";"+super.getJustificacion()+"\n";
+    }
 }
