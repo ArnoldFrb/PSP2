b13faead9bb1df733d1196e1de7a513e0df465a4
SLaxum <55930519+SLaxum@users.noreply.github.com>
2020-12-20 16:01:40 -0500

    Update TareasData.java

--- src/datos/TareasData.java
+++ src/datos/TareasData.java
@@ -89,4 +89,53 @@ public class TareasData {
         }
         return null;
     }
+    
+    public Tarea queryFile(int queryData) throws Exception
+    {
+        List<Tarea> list = new ArrayList();
+        Tarea tarea = new Tarea();
+        boolean flag = false;
+        try
+        {
+            File file = new File("psp2_db\\Ingenieros.txt");
+            FileReader read;
+            BufferedReader buffered;
+            if(file.exists())
+            {
+                read = new FileReader(file);
+                buffered = new BufferedReader(read);
+                
+                String datos;
+                while((datos = buffered.readLine()) != null)
+                {
+                    String[] listDatos = datos.split(";");
+                    if(queryData == Integer.parseInt(listDatos[0]))
+                    {
+                        tarea.setDescripcionTarea(listDatos[0]);
+                        tarea.setDuracionTarea(Integer.parseInt(listDatos[1]));
+                        tarea.setFaseProyecto(listDatos[2]);
+                        tarea.setIngeniero(listDatos[3]);
+                        tarea.setFechaInicio(new SimpleDateFormat("dd/MM/yyyy").parse(listDatos[4]));
+                        tarea.setRolRequerido(listDatos[5]);
+                        tarea.setEstadoTarea(listDatos[6]);
+                        
+                        flag = true;
+                    }
+                }
+                read.close();
+                buffered.close();
+                return tarea;
+            }
+            else
+            {
+                System.out.println("No hay datos");
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("A ocurrido un error");
+            e.printStackTrace();
+        }
+        return flag ? tarea : null;
+    }
 }
