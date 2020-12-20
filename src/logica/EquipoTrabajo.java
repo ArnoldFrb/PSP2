b20e01d5ac675c64fdcd4442d7781d7734902768
luisfersk <39956205+luisfersk@users.noreply.github.com>
2020-12-20 02:37:00 -0500

    Oremos

--- src/logica/EquipoTrabajo.java
+++ src/logica/EquipoTrabajo.java
@@ -1,74 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package logica;
-
 import java.util.ArrayList;
-import java.util.Collections;
-
-/**
- *
- * @author juana
- */
 public class EquipoTrabajo{
-
-    private int idEquipoTrabajo;
-    private String pendientes;
-    private String Ejecutadas;
-    private String ingeniero; 
-    
-    private ArrayList<Tarea> listaTareasPendientes =  new ArrayList<>();
-    private ArrayList<Tarea> listaTareasEjecutadas = new ArrayList<>();
-    private ArrayList<Ingeniero> listaIngenieros;
+    private int identificador;
+    private int identfiacadorEquipoTrabajo;
+    private ArrayList<Tarea> tareas;
+    private ArrayList<Ingeniero> ingenieros;
+    private ArrayList<Tarea> listaTareasPendientes;
+    private ArrayList<Tarea> listaTareasEjecutadas;
 
     public EquipoTrabajo() {
-        this.listaIngenieros = new ArrayList<>();
+        tareas = new ArrayList<Tarea>();
+        ingenieros = new ArrayList<Ingeniero>();
+        listaTareasPendientes = new ArrayList<Tarea>();
+        listaTareasEjecutadas = new ArrayList<Tarea>();
     }
 
-    public ArrayList<Ingeniero> getListaIngenieros() {
-        return listaIngenieros;
+    public EquipoTrabajo(int identificador, int identfiacadorEquipoTrabajo, ArrayList<Tarea> tareas, ArrayList<Ingeniero> ingenieros) {
+        this.identificador = identificador;
+        this.identfiacadorEquipoTrabajo = identfiacadorEquipoTrabajo;
+        this.tareas = tareas;
+        this.ingenieros = ingenieros;
     }
 
-    public void setListaIngenieros(ArrayList<Ingeniero> listaIngenieros) {
-        this.listaIngenieros = listaIngenieros;
-    }
-    
-    public void consultaGeneralTareasPendientes(Proyecto proyecto) {
-        
-        System.out.println("Lista de tareas pendientes: ");
-        for (Tarea tarea : proyecto.getListaTareas()) {
-            if(tarea.getEstadoTarea() == "pendiente")
-            {
-                listaTareasPendientes.add(tarea);
-            }
-        }
-        recorrerLista(listaTareasPendientes);
+    public int getIdentificador() {
+        return identificador;
     }
 
-    public void consultaGeneralTareasRealizadas(Proyecto proyecto) {
-        System.out.println("Lista de tareas ejecutadas: ");
-
-        for (Tarea tarea : proyecto.getListaTareas()) {
-            if(tarea.getEstadoTarea() == "ejecutada")
-            {
-                listaTareasEjecutadas.add(tarea);
-            }
-        }
-
-        recorrerLista(listaTareasPendientes);
+    public int getIdentfiacadorEquipoTrabajo() {
+        return identfiacadorEquipoTrabajo;
     }
 
-    public void FinalizarTarea(Tarea tarea) {
-        tarea.setEstadoTarea("ejecutada");
+    public ArrayList<Tarea> getTareas() {
+        return tareas;
     }
-    
-    public void recorrerLista(ArrayList<Tarea> listaTareas)
-    {
-        for (Tarea tarea : listaTareas) {
-            System.out.println(tarea.toString());
-        }
 
+    public ArrayList<Ingeniero> getIngenieros() {
+        return ingenieros;
+    }
+
+    public ArrayList<Tarea> getListaTareasPendientes() {
+        return listaTareasPendientes;
+    }
+
+    public ArrayList<Tarea> getListaTareasEjecutadas() {
+        return listaTareasEjecutadas;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public void setIdentfiacadorEquipoTrabajo(int identfiacadorEquipoTrabajo) {
+        this.identfiacadorEquipoTrabajo = identfiacadorEquipoTrabajo;
+    }
+
+    public void setTareas(ArrayList<Tarea> tareas) {
+        this.tareas = tareas;
+    }
+
+    public void setListaTareasPendientes(ArrayList<Tarea> tareas) {
+        this.listaTareasPendientes = tareas;
+    }
+
+    public void setListaTareasEjecutadass(ArrayList<Tarea> tareas) {
+        this.listaTareasEjecutadas = tareas;
+    }
+
+    public void setIngenieros(ArrayList<Ingeniero> ingenieros) {
+        this.ingenieros = ingenieros;
     }
 }
